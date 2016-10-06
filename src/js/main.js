35ace8a7bf0019d3c077cb54cef64ff650ae0365
Veronika <mainyvar@gmail.com>
2016-10-06 17:33:51 +0500

    add styles

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,15 @@ $(document).ready(function(){
         $(this).addClass('open');
     });
 
-    $('.newton__menu').on('mouseout', function(){
+    /*$('.newton__menu').on('mouseout', function(){
         $(this).removeClass('open');
+    });*/
+
+    $('#action-block,.discount-menu-block').on('mouseover', function(){
+        $('.discount-menu-block').addClass('hover');
+    });
+
+    $('#action-block,.discount-menu-block').on('mouseout', function(){
+        $('.discount-menu-block').removeClass('hover');
     });
 });
